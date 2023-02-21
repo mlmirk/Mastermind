6250a16bd5f5350aef24d7b367df73c8d67b6d09
mlmirk <mike.l.mirkovic@gmail.com>
2023-02-21 18:26:19 -0500

    add some methods with no game logic yet but starting to work on flow of the user experience

--- src/com/mastermind/app/MastermindApp.java
+++ src/com/mastermind/app/MastermindApp.java
@@ -5,10 +5,21 @@ import java.util.Scanner;
 public class MastermindApp {
     //initialize scanner to capture input from user
     private final Scanner scanner = new Scanner(System.in);
+    private int turn = 0;
+    private final String WELCOME = "Welcome to Mastermind can you guess the code????";
 
     //execute will direct application flow
     public void execute(){
+        getRandomCode();
+        welcome(); 
 
+    }
+
+    private void getRandomCode() {
+    }
+
+    private void welcome() {
+        System.out.println(WELCOME);
     }
 
 }
