eae2f1d4ba896e22364898ed7434d327684d8a2e
mlmirk <mike.l.mirkovic@gmail.com>
2023-02-22 12:07:57 -0500

    create async method with basic try catch to retrive a random number as a string

--- src/com/mastermind/app/MastermindApp.java
+++ src/com/mastermind/app/MastermindApp.java
@@ -1,21 +1,54 @@
 package com.mastermind.app;
 
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 public class MastermindApp {
     //initialize scanner to capture input from user
     private final Scanner scanner = new Scanner(System.in);
     private int turn = 0;
     private final String WELCOME = "Welcome to Mastermind can you guess the code????";
+    private static final HttpClient client = HttpClient.newBuilder()
+            .version(HttpClient.Version.HTTP_1_1)
+            .connectTimeout(Duration.ofSeconds(5))
+            .build();
 
     //execute will direct application flow
     public void execute(){
-        getRandomCode();
+        Thread newThread = new Thread(this::getRandomCode);
+        //create a new thread to fetch the secret number
+        newThread.start();
+
         welcome(); 
 
     }
 
     private void getRandomCode() {
+        //TODO more effective error catching may chaining and priniting based on failure
+       final String url = "https://www.random.org/integers/?num=4&min=0&max=7&col=4&base=10&format=plain&rnd=new";
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(new URI(url))
+                .build();
+        CompletableFuture<HttpResponse<String>> response = null;
+        response = client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        String body = null;
+            body = response.thenApply(HttpResponse::body).get(5, TimeUnit.SECONDS);
+            System.out.println(body);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     private void welcome() {
